e3b93172a54b1e24e26a9c1c097058e6478b8874
Luis Moreno <luis.moreno@edunext.co>
2019-01-22 18:39:59 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='usm_registration_form',
-    version='0.1',
+    version='0.3',
     description='LMS - Registration Extension Form for USM',
     packages=['usm_registration_form'],
     install_requires=[
