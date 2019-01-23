e1e67088ab5e49fed081e65a02585bed87add61e
Luis Moreno <luis.moreno@edunext.co>
2019-01-22 20:50:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 
 import os
+import re
 from setuptools import setup
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+VERSION = get_version('usm_registration_form', '__init__.py')
 
 setup(
     name='usm_registration_form',
-    version='0.3',
+    version=VERSION,
     description='LMS - Registration Extension Form for USM',
     packages=['usm_registration_form'],
     install_requires=[
